ae4fabd75f20690984eed7f210c680af72aa8730
elson <elsonnezaj2005@gmail.com>
2025-02-13 23:18:38 +0100

    finished main header logic

--- header.js
+++ header.js
@@ -1,54 +1,143 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Create the navbar container
-  const navbar = document.createElement("nav");
-  navbar.className = "navbar";
   const fontFaceCSS = `
-  @font-face {
-    font-family: 'Gotham';
-    src: url('https://www.newcleo.com/static/fonts/Gotham-Book.woff2') format('woff2');
-    font-weight: 400;
-    font-style: normal;
-  }
+    @font-face {
+      font-family: 'Gotham';
+      src: url('https://www.newcleo.com/static/fonts/Gotham-Book.woff2') format('woff2');
+      font-weight: 400;
+      font-style: normal;
+    }
 
-  @font-face {
-    font-family: 'Gotham';
-    src: url('https://www.newcleo.com/static/fonts/Gotham-Bold.woff2') format('woff2');
-    font-weight: 700;
-    font-style: normal;
-  }
+    @font-face {
+      font-family: 'Gotham';
+      src: url('https://www.newcleo.com/static/fonts/Gotham-Bold.woff2') format('woff2');
+      font-weight: 700;
+      font-style: normal;
+    }
 
-  @font-face {
-    font-family: 'Gotham';
-    src: url('https://www.newcleo.com/static/fonts/Gotham-BoldItalic.woff2') format('woff2');
-    font-weight: 700;
-    font-style: italic;
-  }
+    @font-face {
+      font-family: 'Gotham';
+      src: url('https://www.newcleo.com/static/fonts/Gotham-BoldItalic.woff2') format('woff2');
+      font-weight: 700;
+      font-style: italic;
+    }
 
-  @font-face {
-    font-family: 'Gotham';
-    src: url('https://www.newcleo.com/static/fonts/Gotham-BookItalic.woff2') format('woff2');
-    font-weight: 400;
-    font-style: italic;
-  }
+    @font-face {
+      font-family: 'Gotham';
+      src: url('https://www.newcleo.com/static/fonts/Gotham-BookItalic.woff2') format('woff2');
+      font-weight: 400;
+      font-style: italic;
+    }
 
-  @font-face {
-    font-family: 'Gotham';
-    src: url('https://www.newcleo.com/static/fonts/Gotham-Ultra.woff2') format('woff2');
-    font-weight: 900;
-    font-style: normal;
-  }
+    @font-face {
+      font-family: 'Gotham';
+      src: url('https://www.newcleo.com/static/fonts/Gotham-Ultra.woff2') format('woff2');
+      font-weight: 900;
+      font-style: normal;
+    }
 
-  @font-face {
-    font-family: 'Gotham';
-    src: url('https://www.newcleo.com/static/fonts/Gotham-Medium.woff2') format('woff2');
-    font-weight: 500;
-    font-style: normal;
-  }
-`;
+    @font-face {
+      font-family: 'Gotham';
+      src: url('https://www.newcleo.com/static/fonts/Gotham-Medium.woff2') format('woff2');
+      font-weight: 500;
+      font-style: normal;
+    }
+  `;
 
   const fontFaceStyle = document.createElement("style");
   fontFaceStyle.textContent = fontFaceCSS;
   document.head.appendChild(fontFaceStyle);
+
+  // Create the navbar container
+  BlackHeader();
+  GreenHeader();
+});
+
+function BlackHeader() {
+  const container = document.createElement("div");
+  container.className = "blackcontainer";
+
+  const items = [
+    {
+      href: "/about-us/newcleo-group/",
+      text: "newcleo Group",
+    },
+    {
+      href: "/press-media/",
+      text: "Press and media",
+    },
+    {
+      href: "/supply-chain/",
+      text: "Supply Chain",
+    },
+    {
+      href: "https://www.newcleo.com/investors/",
+      text: "Investors",
+    },
+    {
+      href: "/contact-us/",
+      text: "Contact us",
+    },
+  ];
+
+  const navContainer = document.createElement("div");
+  navContainer.className = "navContainer";
+
+  container.appendChild(navContainer);
+
+  items.map((item) => {
+    const singleItem = document.createElement("a");
+    singleItem.className = "singleItem";
+    singleItem.href = item.href;
+    singleItem.innerHTML = item.text;
+
+    navContainer.appendChild(singleItem);
+  });
+
+  document.body.appendChild(container);
+
+  const blackContainerStyles = document.createElement("style");
+  blackContainerStyles.textContent = `
+          .blackcontainer {
+            height:40px;
+            background-color:#333;
+            padding: 0px 230px;
+          }
+
+          .navContainer {
+            display:flex;
+            height:100%;
+            width:100%;
+            align-items:center;
+            justify-content:flex-end;
+            gap:10px;
+            position:relative;
+            right:15px;
+          }
+
+          .singleItem {
+            color:white !important;
+            text-decoration: none !important;
+            font-size:12px;
+            font-weight:bold;
+            padding:0px 10px;
+            cursor:pointer;
+            transition: all 0.5s;
+            display:flex;
+            align-items:center;
+            height:100%;
+          }
+
+          .singleItem:hover{
+            background-color:rgba(255, 255, 255, 0.15);
+          }
+
+      `;
+  document.head.appendChild(blackContainerStyles);
+}
+
+function GreenHeader() {
+  const navbar = document.createElement("nav");
+  navbar.className = "navbar";
 
   // Create the logo
   const logo = document.createElement("div");
@@ -59,7 +148,6 @@ document.addEventListener("DOMContentLoaded", function () {
     "https://images.ctfassets.net/26ep875sz1dl/5tqbcXMxG54o08zYxhm3CT/0d9dd19f38a7fdd95bc2d8c1a9fd57c5/logo_white1__1_.svg";
   logoImg.alt = "newcleo Logo";
   logoImg.style.height = "40px";
-  logoImg.style.width = "7.6rem";
 
   logo.appendChild(logoImg);
   navbar.appendChild(logo);
@@ -195,106 +283,107 @@ document.addEventListener("DOMContentLoaded", function () {
   // Add styles dynamically
   const style = document.createElement("style");
   style.textContent = `
-    body {
-       font-family: 'Gotham', Arial, sans-serif;
-        margin: 0;
-        padding: 0;
-    }
-
-    .navbar {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        background-color:rgb(2, 81, 82);
-        padding: 10px 220px;
-    }
-
-    .nav-links {
-        list-style: none;
-        display: flex;
-        gap:25px;
-        margin: 0;
-        padding: 0;
-    }
-
-    .nav-links li {
-        position: relative;
-        display:flex;
-        flex-direction: row;
-        align-items:center;
-        gap:5px;
-    }
-
-    .arrow {
-        color: white;
-        text-decoration: none;
-        display: block;
-        transition: all 0.3s;
-    }
-
-    .nav-links li a {
-        color: white;
-        text-decoration: none;
-        display: block;
-        transition: all 0.3s;
-        font-size: 12px;
-    }
-
-    .arrow { 
-      font-size: 12px;
-      display:flex;
-      align-items:center;
-      color: white;
-      text-decoration: none;
-      transition: all 0.3s;
-      position:relative;
-      top:-5px;
-    }
-
-    .nav-links li:hover {
-      .linkLabel {
-        color:rgb(237, 108, 34);
-      };
-      .arrow {
-          color:rgb(237, 108, 34);
-          rotate: 180deg;
-          top:5px;
+      body {
+        font-family: 'Gotham', Arial, sans-serif;
+          margin: 0;
+          padding: 0;
       }
-    }
 
-    .dropdown {
-        display: none;
-        position: absolute;
-        top: 100%;
-        left: 0;
-        background-color: #444;
-        list-style: none;
-        padding: 0;
-        margin: 0;
-        min-width: 200px;
-    }
+      .navbar {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          background-color:rgb(2, 81, 82);
+          padding: 10px 150px;
+          border-radius:0px;
+      }
 
-    .dropdown li {
-     transition: all 0.2s;
-     cusor:pointer;
-      display:flex;
-      justify-content:center;
-    }
+      .nav-links {
+          list-style: none;
+          display: flex;
+          gap:25px;
+          margin: 0;
+          padding: 0;
+      }
 
-    .dropdown li a {
-        width: 100%;
-        padding: 10px;
-        display: flex;
-        justify-content:flex-start;
-        font-size: 14px;
-        border-bottom: solid 1px grey;
-    }
+      .nav-links li {
+          position: relative;
+          display:flex;
+          flex-direction: row;
+          align-items:center;
+          gap:5px;
+      }
 
-    .dropdown li:hover {
-      background-color: rgb(148, 148, 148) !important;
-      color: rgb(198, 198, 198) !important;
-    }
-`;
+      .arrow {
+          color: white;
+          text-decoration: none;
+          display: block;
+          transition: all 0.3s;
+      }
+
+      .nav-links li a {
+          color: white;
+          text-decoration: none;
+          display: block;
+          transition: all 0.3s;
+          font-size: 12px;
+      }
+
+      .arrow { 
+        font-size: 12px;
+        display:flex;
+        align-items:center;
+        color: white;
+        text-decoration: none;
+        transition: all 0.3s;
+        position:relative;
+        top:-5px;
+      }
+
+      .nav-links li:hover {
+        .linkLabel {
+          color:rgb(237, 108, 34);
+        };
+        .arrow {
+            color:rgb(237, 108, 34);
+            rotate: 180deg;
+            top:5px;
+        }
+      }
+
+      .dropdown {
+          display: none;
+          position: absolute;
+          top: 100%;
+          left: 0;
+          background-color: #444;
+          list-style: none;
+          padding: 0;
+          margin: 0;
+          min-width: 200px;
+      }
+
+      .dropdown li {
+      transition: all 0.2s;
+      cusor:pointer;
+        display:flex;
+        justify-content:center;
+      }
+
+      .dropdown li a {
+          width: 100%;
+          padding: 10px;
+          display: flex;
+          justify-content:flex-start;
+          font-size: 14px;
+          border-bottom: solid 1px grey;
+      }
+
+      .dropdown li:hover {
+        background-color: rgb(148, 148, 148) !important;
+        color: rgb(198, 198, 198) !important;
+      }
+  `;
 
   document.head.appendChild(style);
-});
+}
