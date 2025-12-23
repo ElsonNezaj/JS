be56b21bcc8148b3670e38537e98346aeb0e9548
erbiguza <erbiguza@gmail.com>
2025-12-23 15:12:54 +0100

    finished locations page with all its stylings

--- locations.js
+++ locations.js
@@ -1,4 +1,6 @@
 const slides = document.querySelectorAll(".slides img");
+const names = document.querySelectorAll(".location-slider h2");
+const descriptions = document.querySelectorAll(".location-slider h4");
 let slideIndex = 0;
 let intervalId = null;
 
@@ -8,6 +10,8 @@ document.addEventListener("DOMContentLoaded", initializeSlider);
 function initializeSlider(){
     if(slides.length>0){
         slides[slideIndex].classList.add("show");
+        names[slideIndex].classList.add("show");
+        descriptions[slideIndex].classList.add("show");
         intervalId = setInterval(nextSlide, 5000);
     }
 }
@@ -22,8 +26,16 @@ function showSlide(index){
     slides.forEach(slide => {
         slide.classList.remove("show");
     });
+    names.forEach(name => {
+        name.classList.remove("show");
+    });
+    descriptions.forEach(description => {
+        description.classList.remove("show");
+    })
 
     slides[slideIndex].classList.add("show");
+    names[slideIndex].classList.add("show");
+    descriptions[slideIndex].classList.add("show");
 }
 
 function prevSlide(){
@@ -39,3 +51,33 @@ function nextSlide(){
 function stopTimer(){
     clearInterval(intervalId);
 }
+
+const dateInput = document.getElementById("date-picker");
+const today = new Date();
+
+const yyyy = today.getFullYear();
+const mm = String(today.getMonth()+1).padStart(2, '0');
+const dd = String(today.getDate()+1).padStart(2, '0');
+
+const formattedDate = `${yyyy}-${mm}-${dd}`;
+dateInput.min = formattedDate;
+
+function clearForm(){
+    const nameInput = document.getElementById("name-input");
+    const emailInput = document.getElementById("email-input");
+    const messageInput = document.getElementById("message");
+    const appointmentType = document.getElementById("appointment-type");
+    const locationSelect = document.getElementById("location");
+
+    nameInput.value = '';
+    emailInput.value = '';
+    messageInput.value = '';
+    appointmentType.value = 'a';
+    locationSelect.value = 'a';
+    dateInput.value = formattedDate;
+}
+
+const locationText = document.getElementById("location-text");
+const txt = ['White Hills Wind', 'Big Cypress Solar', 'Alternative Energy&Services', 'Itaipu Dam', 'Butler Ridge Wind'];
+
+locationText.value = txt[slideIndex];
