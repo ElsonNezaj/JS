befc739d129c75367dc67664a8408fff76175073
Erbi Guza <erbiguza@gmail.com>
2026-01-02 16:27:40 +0100

    finished background image bugs on windows for locations.html

--- locations.js
+++ locations.js
@@ -1,54 +1,71 @@
-const slides = document.querySelectorAll(".slides img");
 const names = document.querySelectorAll(".location-slider h2");
 const descriptions = document.querySelectorAll(".location-slider h4");
+const slideDiv = document.getElementById("slide");
 let slideIndex = 0;
 let intervalId = null;
 
+const images = [
+    "./assets/images/windFarmArizona.jpg",
+    "./assets/images/solarFarmArkansas.jpg",
+    "./assets/images/solarFarmColorado.jpg",
+    "./assets/images/hydropower.jpeg",
+    "./assets/images/winsconsonWindFarm.jpg",
+];
 
 document.addEventListener("DOMContentLoaded", initializeSlider);
 
-function initializeSlider(){
-    if(slides.length>0){
-        slides[slideIndex].classList.add("show");
+slideDiv.addEventListener("click", () => {
+    window.location.href = "#locations";
+});
+
+if (!slideDiv) {
+    console.log("no slide div");
+} else {
+    console.log("yes slide div");
+}
+
+function initializeSlider() {
+    if (images.length > 0) {
+        slideDiv.style.backgroundImage = `url(${images[slideIndex]})`;
+        slideDiv.style.backgroundSize = "cover";
+        slideDiv.style.backgroundPosition = "center";
+        slideDiv.style.backgroundRepeat = "no-repeat";
         names[slideIndex].classList.add("show");
         descriptions[slideIndex].classList.add("show");
         intervalId = setInterval(nextSlide, 5000);
     }
 }
 
-function showSlide(index){
-    if(index >= slides.length){
+function showSlide(index) {
+    if (index >= images.length) {
         slideIndex = 0;
-    }else if (index < 0){
-        slideIndex = slides.length - 1;
+    } else if (index < 0) {
+        slideIndex = images.length - 1;
     }
 
-    slides.forEach(slide => {
-        slide.classList.remove("show");
-    });
-    names.forEach(name => {
+    slideDiv.style.backgroundImage = `url(${images[slideIndex]})`;
+    names.forEach((name) => {
         name.classList.remove("show");
     });
-    descriptions.forEach(description => {
+    descriptions.forEach((description) => {
         description.classList.remove("show");
-    })
+    });
 
-    slides[slideIndex].classList.add("show");
     names[slideIndex].classList.add("show");
     descriptions[slideIndex].classList.add("show");
 }
 
-function prevSlide(){
+function prevSlide() {
     slideIndex--;
     showSlide(slideIndex);
 }
 
-function nextSlide(){
+function nextSlide() {
     slideIndex++;
     showSlide(slideIndex);
 }
 
-function stopTimer(){
+function stopTimer() {
     clearInterval(intervalId);
 }
 
@@ -56,23 +73,23 @@ const dateInput = document.getElementById("date-picker");
 const today = new Date();
 
 const yyyy = today.getFullYear();
-const mm = String(today.getMonth()+1).padStart(2, '0');
-const dd = String(today.getDate()+1).padStart(2, '0');
+const mm = String(today.getMonth() + 1).padStart(2, "0");
+const dd = String(today.getDate() + 1).padStart(2, "0");
 
 const formattedDate = `${yyyy}-${mm}-${dd}`;
 dateInput.min = formattedDate;
 
-function clearForm(){
+function clearForm() {
     const nameInput = document.getElementById("name-input");
     const emailInput = document.getElementById("email-input");
     const messageInput = document.getElementById("message");
     const appointmentType = document.getElementById("appointment-type");
     const locationSelect = document.getElementById("location");
 
-    nameInput.value = '';
-    emailInput.value = '';
-    messageInput.value = '';
-    appointmentType.value = 'a';
-    locationSelect.value = 'a';
+    nameInput.value = "";
+    emailInput.value = "";
+    messageInput.value = "";
+    appointmentType.value = "a";
+    locationSelect.value = "a";
     dateInput.value = formattedDate;
 }
